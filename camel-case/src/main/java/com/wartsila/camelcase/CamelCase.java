c1021cfaa2ec40f603bba12781e44de98f143a32
Michael Melsen <michael.melsen@wartsila.com>
2020-07-03 08:39:43 +0200

    Camel case solution added

--- camel-case/src/main/java/com/wartsila/camelcase/CamelCase.java
+++ camel-case/src/main/java/com/wartsila/camelcase/CamelCase.java
@@ -1,8 +1,44 @@
 package com.wartsila.camelcase;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class CamelCase {
 
     static String toCamelCase(String s){
-        return "";
+
+        char[] chars = s.toCharArray();
+
+        boolean toUppercase = false;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+
+            char aChar = chars[i];
+            if(aChar == '-' || aChar == '_') {
+                toUppercase = true;
+            }
+            else {
+                stringBuilder.append(toUppercase ? Character.toUpperCase(aChar) : aChar);
+                toUppercase = false;
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    static String toCamelCaseUsingRegex(String s) {
+
+        String[] split = s.split("[_\\-]+");
+
+        List<String> toBeJoined = new ArrayList<>();
+        for (int i = 0; i < split.length; i++) {
+            String str = split[i];
+            String r = str;
+            if(i>0)
+                r = str.substring(0, 1).toUpperCase() + str.substring(1);
+            toBeJoined.add(r);
+
+        }
+        return toBeJoined.stream().collect(Collectors.joining());
     }
 }
--- camel-case/src/test/java/com/wartsila/camelcase/CamelCaseTest.java
+++ camel-case/src/test/java/com/wartsila/camelcase/CamelCaseTest.java
@@ -6,16 +6,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CamelCaseTest {
     @Test
-    public void camelCase_stringWithUnderscoresLowerStart_success() {
+    public void camelCase_stringWithUnderscoresLowercaseStart_success() {
         String input = "digital_Product_Development";
         System.out.println("input: " + input);
         assertEquals("digitalProductDevelopment", CamelCase.toCamelCase(input));
     }
 
     @Test
-    public void camelCase_testSomeDashLowerStart_success() {
+    public void camelCase_testSomeDashLowercaseStart_success() {
         String input = "digital-Product-Development";
         System.out.println("input: " + input);
         assertEquals("digitalProductDevelopment", CamelCase.toCamelCase(input));
     }
+
+    @Test
+    public void camelCase_stringWithUnderscoresUppercaseStart_success() {
+        String input = "Digital_Product_Development";
+        System.out.println("input: " + input);
+        assertEquals("DigitalProductDevelopment", CamelCase.toCamelCase(input));
+    }
+
+    @Test
+    public void camelCase_testSomeDashUppercaseStart_success() {
+        String input = "Digital-Product-Development";
+        System.out.println("input: " + input);
+        assertEquals("DigitalProductDevelopment", CamelCase.toCamelCase(input));
+    }
+
 }
